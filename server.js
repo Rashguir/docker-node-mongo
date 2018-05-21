71188a72687923cc11f3376afff08d1d3cb6949e
Nicolas.hedoire <nicolashedoire@hotmail.com>
2018-05-21 14:28:52 +0200

    add socket.io

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(PORT, function(){
